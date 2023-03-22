940f31a58e87c9e91a66a846b8357e6294b4066d
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 20:11:32 +0200

    updated OurRangeCard.jsx

--- src/components/OurRange/OurRangeCard.jsx
+++ src/components/OurRange/OurRangeCard.jsx
@@ -7,5 +7,20 @@ export const OurRangeCard = ({
   imgSrc,
   delay,
 }) => {
-  return <div>OurRangeCard</div>;
+  return (
+    <div
+      className="rounded-[24px] bg-white p-[40px]"
+      style={{
+        boxShadow: "0px 30px 60px rgba(163, 168, 179, 0.3)",
+        flex: "0 1 33.3%",
+      }}
+    >
+      <h3 className="mb-2 text-[36px] font-semibold leading-[144%] text-black">
+        {title}
+      </h3>
+      <p className="paragraph">{description}</p>
+      <img className="my-[60px] mx-auto" src={imgSrc} alt="icon" />
+      <button className="btn mx-auto">{btnText}</button>
+    </div>
+  );
 };
