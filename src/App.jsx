c4e16abd49f4b5245da4d4ca774cf2e64084d944
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 17:54:31 +0200

    created Intro.jsx & added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { Intro } from "./components/Intro/Intro";
 
 function App() {
   return (
     <>
       <Header />
+      <Intro />
     </>
   );
 }
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../Container";
+
+export const Intro = () => {
+  return (
+    <section id="intro">
+      <Container>
+        <div className=""></div>
+      </Container>
+    </section>
+  );
+};
