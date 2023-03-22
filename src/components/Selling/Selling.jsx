c48062f8bc7cbd5859c94b43cbff9561b086d2d9
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 21:15:11 +0200

    added decor

--- src/components/Selling/Selling.jsx
+++ src/components/Selling/Selling.jsx
@@ -6,8 +6,21 @@ import { SellingCard } from "./SellingCard";
 
 export const Selling = () => {
   return (
-    <section id="selling">
+    <section id="selling" className="relative z-0">
       <Container>
+        <div
+          className="absolute -top-[100px] -right-[0] z-[-1] w-[400px] scale-x-[-1] max-sm:hidden"
+          style={{
+            filter:
+              "blur(7.5px) drop-shadow(-15px 50px 70px rgba(27, 27, 29, 0.1))",
+          }}
+        >
+          <img
+            className="w-full object-contain"
+            src="/images/decorate/decorate-coffee-cup.png"
+            alt="decorate coffee cup"
+          />
+        </div>
         <SectionHeader
           title={"Best Selling Coffee"}
           description={
