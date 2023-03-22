6f2f49040544c8e8ffced4e5253d1cd938096a97
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 17:49:45 +0200

    commit

---
+++ src/db.js
@@ -0,0 +1,18 @@
+export const navList = [
+  {
+    title: "Cafe Munu",
+    href: "#",
+  },
+  {
+    title: "About Us",
+    href: "#",
+  },
+  {
+    title: "Find Us",
+    href: "#",
+  },
+  {
+    title: "Alowishus Catering",
+    href: "#",
+  },
+];
