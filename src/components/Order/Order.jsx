6d81659ae8d49a8320cd2fb85de01f2aec112a4c
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 20:35:12 +0200

    updated Order.jsx

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -5,7 +5,24 @@ export const Order = () => {
   return (
     <section id="order">
       <Container>
-        <div className="flex items-center justify-center"></div>
+        <div className="flex items-center justify-center">
+          <img
+            className="w-full max-w-[700px]"
+            src="/images/decorate/decorate-coffee-cups.png"
+            alt="decorate coffee cups"
+          />
+          <div className="w-full max-w-[512px]">
+            <h2 className="section-title mb-[30px]">
+              Order Your Favourite Coffee
+            </h2>
+            <p className="paragraph mb-[60px]">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+            <button className="btn">Order Now</button>
+          </div>
+        </div>
       </Container>
     </section>
   );
