0463e52cff3a63c2bd8ccee1d5a689b579de057b
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 17:52:13 +0200

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,14 +8,18 @@ export const Header = () => {
     <header className="fixed left-0 top-0 w-full py-[24px]">
       <Container>
         <div className="flex items-center justify-between gap-[30px]">
+          {/* logo left */}
           <img
             className="w-full max-w-[126px] cursor-pointer max-sm:max-w-[100px]"
             src="/images/icons/logo.svg"
             alt="logo"
           />
+          {/* right */}
           <div className="flex items-center gap-[40px]">
             <Navbar itemsList={navList} />
+            {/* buttons box */}
             <div className="flex items-center gap-[20px]">
+              {/* cart button */}
               <button className="relative flex h-[56px] w-[56px] items-center justify-center rounded-full bg-white transition duration-300 sm:hover:scale-105">
                 <img src="/images/icons/cart.svg" alt="cart icon" />
               </button>
