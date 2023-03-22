97b2a5e8441e331bff8bb63067926498ec20d6e0
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 21:08:29 +0200

    updated Selling.jsx

--- src/components/Selling/Selling.jsx
+++ src/components/Selling/Selling.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { sellingCards } from "../../db";
 import { Container } from "../Container";
 import { SectionHeader } from "../SectionHeader";
+import { SellingCard } from "./SellingCard";
 
 export const Selling = () => {
   return (
@@ -12,7 +14,18 @@ export const Selling = () => {
             "A drink from the 'My Alowishus' bottled brews range OR grab one of our delicious coffee's."
           }
         />
-        <div className=""></div>
+        <div className="mt-[180px] flex justify-center gap-[30px] max-md:flex-wrap">
+          {sellingCards.map(({ title, description, price, imgSrc }, i) => (
+            <SellingCard
+              key={i}
+              num={i}
+              title={title}
+              description={description}
+              price={price}
+              imgSrc={imgSrc}
+            />
+          ))}
+        </div>
       </Container>
     </section>
   );
