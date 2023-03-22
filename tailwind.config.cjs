b573d948d6966bb0b62afb0aa816b68317add1a5
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 16:59:35 +0200

    updated tailwind.config,cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        black: "#1b1b1d",
+        darkGray: "#64646a",
+        gray: "#eeeff1",
+        lightGrey: "#f3f4f5",
+        white: "#ffffff",
+      },
     },
     screens: {
       xs: "480px",
