958721d298d597681892b705f507f6dc96f20989
ViniVanC <vincentvan0302@gmail.com>
2023-03-22 19:53:28 +0200

    added array ourRangeCards to db.js

--- src/db.js
+++ src/db.js
@@ -16,3 +16,24 @@ export const navList = [
     href: "#",
   },
 ];
+
+export const ourRangesCards = [
+  {
+    title: "Our Catering",
+    description: "The Food",
+    btnText: "The Gelato",
+    imgSrc: "/images/icons/food.png",
+  },
+  {
+    title: "Alowishus Catering, delicious drop off Catering",
+    description: "Our entire menu is available as dine in or takeaway.",
+    btnText: "Life is like GELATO, enjoy it before it melts.",
+    imgSrc: "/images/icons/fast-food.png",
+  },
+  {
+    title: "Order cathering",
+    description: "food menu",
+    btnText: "Discover more",
+    imgSrc: "/images/icons/ice-cream.png",
+  },
+];
