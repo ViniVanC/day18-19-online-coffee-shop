6e162012e23f81fc0b37e8d6101fa13ec61b6808
ViniVanC <vincentvan0302@gmail.com>
2023-03-23 15:02:39 +0200

    commit

--- src/db.js
+++ src/db.js
@@ -77,4 +77,18 @@ export const reviewsCards = [
     stars: 5,
     imgSrc: "/images/reviews/user-img-reviews2.png",
   },
+  {
+    title: "Brooklyn Simmons",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia",
+    stars: 5,
+    imgSrc: "/images/reviews/user-img-reviews1.png",
+  },
+  {
+    title: "Imam White",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia",
+    stars: 5,
+    imgSrc: "/images/reviews/user-img-reviews2.png",
+  },
 ];
